651601f5db7fa24e0c19c1083d387f4d3938aefa
artem <artem131103@yandex.ru>
2023-07-12 17:40:30 +0300

    fix aside and add script video

--- js/burger.js
+++ js/burger.js
@@ -1,36 +1,32 @@
 const burger = document.querySelector(".header__burger");
 const aside = document.querySelector(".aside");
+const asideWrapper = document.querySelector('.aside__wrapper');
 
-if (window.innerWidth > 1200) {
-  if (!aside.classList.contains("aside__activeTwo")) {
-    burger.classList.add("burgerActive");
-  }
-
-  burger.addEventListener("click", () => {
-    aside.classList.toggle("aside__activeTwo");
-    if (aside.classList.contains("aside__activeTwo")) {
-      burger.classList.remove("burgerActive");
-    } else {
-      burger.classList.add("burgerActive");
-    }
-  });
-} else {
-
-  aside.addEventListener("scroll", () => {
-    if(aside.scrollTop > 0) {
-      burger.style.opacity = "0";
-    }else{
-      burger.style.opacity = "1";
-    }
-  })
   burger.addEventListener("click", () => {
     burger.classList.toggle("burgerActive");
     aside.classList.toggle("aside__active");
-
+    asideWrapper.classList.toggle('asideActive')
     if (burger.classList.contains("burgerActive")) {
       document.body.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "auto";
     }
   });
-}
+
+
+aside.addEventListener("scroll", () => {
+  if(aside.scrollTop > 0) {
+    burger.style.opacity = "0";
+  }else{
+    burger.style.opacity = "1";
+  }
+})
+
+asideWrapper.addEventListener("click", (event) => {
+  if(event.target === asideWrapper){
+    burger.classList.remove("burgerActive");
+    aside.classList.remove("aside__active");
+    asideWrapper.classList.remove('asideActive')
+    document.body.style.overflow = 'auto';
+  }
+})
---
+++ js/video.js
@@ -0,0 +1,24 @@
+const video = document.querySelector('.video > video');
+const moreVideoBlock = document.querySelector('.more__video-block')
+
+
+
+function videoResize() {
+  console.log(window.innerWidth);
+  console.log(window.innerWidth - 40 - 350 - 30);
+  if(window.innerWidth > 1920) return
+  let videoWidth = window.innerWidth - 40 - 350 - 30
+  if(window.innerWidth <= 1200){
+    videoWidth = window.innerWidth - 24
+  }
+  let videoHeight = videoWidth / 2
+  console.log(videoWidth,videoHeight);
+  video.style.width = `${videoWidth}px`
+  video.style.height = `${videoHeight}px`
+  moreVideoBlock.style.maxHeight = `${videoHeight - 36}px`
+}
+
+
+videoResize()
+
+window.addEventListener("resize", videoResize)
